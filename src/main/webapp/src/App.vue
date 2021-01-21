4d639a48d6afc15e050563b62df5b6cdd2751892
jiangqiang <jiangqiang110@126.com>
2021-01-21 16:27:22 +0800

    bug fix

--- src/main/webapp/src/App.vue
+++ src/main/webapp/src/App.vue
@@ -5,13 +5,12 @@
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 import Home from '@/views/Home.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld,Home
+    Home
   }
 }
 </script>
