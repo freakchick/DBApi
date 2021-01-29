cb77c8292e7eb9570305e97f73a45009a577b9e6
jiangqiang <jiangqiang110@126.com>
2021-01-29 10:24:42 +0800

    启动脚本修复

--- src/main/bin/dbApi.sh
+++ src/main/bin/dbApi.sh
@@ -1,23 +1,23 @@
 #!/bin/sh
 
-pid=server.pid
+BIN_DIR=$(dirname $0)
+BIN_DIR=$(
+  cd "$BIN_DIR"
+  pwd
+)
+HOME=$BIN_DIR/..
+PID=$BIN_DIR/server.pid
+
+export CONF_DIR=$HOME/conf
+export LIB_JARS=$HOME/lib/*
 
 if [ $1 = "start" ]; then
-  BIN_DIR=$(dirname $0)
-  BIN_DIR=$(
-    cd "$BIN_DIR"
-    pwd
-  )
-  HOME=$BIN_DIR/..
 
-  export CONF_DIR=$HOME/conf
-  export LIB_JARS=$HOME/lib/*
-
-  nohup java -classpath $CONF_DIR:$LIB_JARS com.jq.dbapi.DBApiApplication > /dev/null 2>&1 &
-  echo $! >$pid
+  nohup java -classpath $CONF_DIR:$LIB_JARS com.jq.dbapi.DBApiApplication >/dev/null 2>&1 &
+  echo $! >$PID
 
 elif [ $1 = "stop" ]; then
-  TARGET_PID=$(cat $pid)
+  TARGET_PID=$(cat $PID)
   kill $TARGET_PID
 
 else
