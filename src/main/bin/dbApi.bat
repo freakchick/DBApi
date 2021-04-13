51e10ecd1b658e58e87388c263c9ac9fc0ce99fc
jiangqiang <1032355091@qq.com>
2021-04-13 19:57:27 +0800

    bat 脚本

--- src/main/bin/dbApi.bat
+++ src/main/bin/dbApi.bat
@@ -1,13 +1,9 @@
 @echo off
 set home=%~dp0
-echo %home%
 
 set conf_dir=%home%..\conf
 set lib_dir=%home%..\lib\*
+set log_dir=%home%..\logs
 
-echo %conf_dir%
-echo %lib_dir%
-
-echo "java -classpath %conf_dir%;%lib_dir% com.jq.dbapi.DBApiApplication"
-
-java -classpath %conf_dir%;%lib_dir% com.jq.dbapi.DBApiApplicationn
+java -Dlogging.file=%log_dir%\dbApi.log -classpath %conf_dir%;%lib_dir% com.jq.dbapi.DBApiApplication
+pause
