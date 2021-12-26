0f6c5a52feb516987d213be3843258315ac1628f
jiangqiang <jiangqiang110@126.com>
2021-12-26 21:15:08 +0800

    gateway 打包集成

--- dbapi-cluster-gateway/src/main/java/com/gitee/freakchicken/dbapi/DBApiGateWay.java
+++ dbapi-cluster-gateway/src/main/java/com/gitee/freakchicken/dbapi/DBApiGateWay.java
@@ -2,12 +2,17 @@ package com.gitee.freakchicken.dbapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
-//@PropertySource("application-gateway.yml")
+//@ComponentScan(value = "com.gitee.freakchicken.dbapi", excludeFilters = {@ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = {APIFilter.class})})  //filter会自动注册
+//@PropertySource("application-gateway.properties")
 public class DBApiGateWay {
     public static void main(String[] args) {
+        System.setProperty("spring.profiles.active","gateway");
 
         SpringApplication.run(DBApiGateWay.class, args);
     }
