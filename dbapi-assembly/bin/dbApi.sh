3e1ce3d46d0b73a9a307b9cce29d603eb8fb064c
jiangqiang <jiangqiang110@126.com>
2021-12-27 20:01:34 +0800

    gateway 打包集成

--- dbapi-assembly/bin/dbApi.sh
+++ dbapi-assembly/bin/dbApi.sh
@@ -2,16 +2,17 @@
 
 bool=false
 
-while getopts d opt;
-do
-case $opt in
-  d) echo "running in daemon"
+while getopts d opt; do
+  case $opt in
+  d)
+    echo "running in daemon"
     bool=true
     shift
     ;;
-  ?) echo "$opt is an invalid option"
+  ?)
+    echo "$opt is an invalid option"
     ;;
-esac
+  esac
 done
 
 BIN_DIR=$(dirname $0)
@@ -27,11 +28,21 @@ export CONF_DIR=$HOME/conf
 export LIB_DIR=$HOME/lib/
 export LOG_DIR=$HOME/logs
 
+# 判断第一个参数包含第二个参数开头的字符串
+function contain() {
+  array=$1
+  for item in ${array[*]}; do
+    if [[ $2 =~ ^"${item}-".* ]]; then
+      echo $2
+      return 0
+    fi
+  done
+  return 1
+}
 
 standalone_cp=$CONF_DIR
 # shellcheck disable=SC2045
-for tmp in $(ls $LIB_DIR)
-do
+for tmp in $(ls $LIB_DIR); do
   if [[ $tmp != dbapi-cluster-* ]]; then
     standalone_cp=$standalone_cp:$LIB_DIR$tmp
   fi
@@ -39,32 +50,27 @@ done
 
 manager_cp=$CONF_DIR
 # shellcheck disable=SC2045
-for tmp in $(ls $LIB_DIR)
-do
-  if [[ $tmp != dbapi-standalone-* ]] && [[ $tmp != dbapi-cluster-apiServer-* ]] ; then
+for tmp in $(ls $LIB_DIR); do
+  if [[ $tmp != dbapi-standalone-* ]] && [[ $tmp != dbapi-cluster-apiServer-* ]]; then
     manager_cp=$manager_cp:$LIB_DIR$tmp
   fi
 done
 
 api_cp=$CONF_DIR
 # shellcheck disable=SC2045
-for tmp in $(ls $LIB_DIR)
-do
-  if [[ $tmp != dbapi-standalone-* ]] && [[ $tmp != dbapi-cluster-manager-* ]] ; then
+for tmp in $(ls $LIB_DIR); do
+  if [[ $tmp != dbapi-standalone-* ]] && [[ $tmp != dbapi-cluster-manager-* ]]; then
     api_cp=$api_cp:$LIB_DIR$tmp
   fi
 done
 
-gateway_cp=$CONF_DIR/application.yml
-# shellcheck disable=SC2045
-for tmp in $(ls $LIB_DIR)
-do
-  if [[ $tmp != dbapi-* ]] ; then
-    gateway_cp=$gateway_cp:$LIB_DIR$tmp
-  fi
-
-  if [[ $tmp == dbapi-cluster-gateway-* ]] ; then
-    gateway_cp=$gateway_cp:$LIB_DIR$tmp
+exclude_jars=("spring-boot-starter-tomcat" "spring-boot-starter-web" "tomcat-embed-websocket" "tomcat-embed-core" "spring-webmvc" "dbapi-cluster-apiServer" "dbapi-cluster-manager" "dbapi-controller" "dbapi-standalone")
+gateway_cp=$CONF_DIR
+for tmp in $(ls $LIB_DIR); do
+  contain "${exclude_jars[*]}" $tmp
+  res=$(echo $?)
+  if [ $res = "1" ]; then
+    gateway_cp=$gateway_cp:$LIB_DIR$tmp #不包含在其中就拼接
   fi
 done
 
