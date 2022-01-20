3cb9c91f2d5a2bd54875bfab207f8ce405026d44
jiangqiang <jiangqiang110@126.com>
2022-01-20 20:55:32 +0800

    exclude class

--- dbapi-cluster-gateway/src/main/java/com/gitee/freakchicken/dbapi/DBApiGateWay.java
+++ dbapi-cluster-gateway/src/main/java/com/gitee/freakchicken/dbapi/DBApiGateWay.java
@@ -1,18 +1,23 @@
 package com.gitee.freakchicken.dbapi;
 
-import com.gitee.freakchicken.dbapi.filter.APIFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
-import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
-@ComponentScan(value = "com.gitee.freakchicken.dbapi", excludeFilters = {@ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = {APIFilter.class})})  //filter会自动注册
+@ComponentScan(value = "com.gitee.freakchicken.dbapi", excludeFilters = {
+        @ComponentScan.Filter(type = FilterType.REGEX, pattern = {
+                "com.gitee.freakchicken.dbapi.dao.*",
+                "com.gitee.freakchicken.dbapi.filter.*",
+                "com.gitee.freakchicken.dbapi.service.*",
+                "com.gitee.freakchicken.dbapi.servlet.*"
+        })
+})
 //@PropertySource("classpath:application-gateway.yml")
 public class DBApiGateWay {
     public static void main(String[] args) {
-        System.setProperty("spring.profiles.active","gateway");
+        System.setProperty("spring.profiles.active", "gateway");
 
         SpringApplication.run(DBApiGateWay.class, args);
     }
