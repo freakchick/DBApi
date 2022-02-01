74fda22761fb9980be331f58cd4c0c73895dab34
jiangqiang <1032355091@qq.com>
2022-02-02 06:54:08 +0800

    集群启动脚本

--- dbapi-assembly/bin/start-all.sh
+++ dbapi-assembly/bin/start-all.sh
@@ -2,19 +2,20 @@
 
 workDir=`dirname $0`
 workDir=`cd ${workDir};pwd`
+DBAPI_HOME=`cd "$workDir/.."; pwd`
 
-source $workDir/../conf/install_config.sh
+source $DBAPI_HOME/conf/install_config.conf
 
 apiServerHost=(${apiServers//,/ })
 for apiServer in ${apiServerHost[@]}
 do
   echo "$apiServer api server is starting"
-	ssh -p $sshPort $apiServer  "cd $installPath/; sh bin/dbapi-daemon.sh start apiServer;"
+	ssh -p $sshPort $apiServer  "cd $DBAPI_HOME/; sh bin/dbapi-daemon.sh start apiServer;"
 done
 
-ssh -p $sshPort $manager  "cd $installPath/; sh bin/dbapi-daemon.sh start manager;"
+ssh -p $sshPort $manager  "cd $DBAPI_HOME/; sh bin/dbapi-daemon.sh start manager;"
 
-ssh -p $sshPort $gateway  "cd $installPath/; sh bin/dbapi-daemon.sh start gateway;"
+ssh -p $sshPort $gateway  "cd $DBAPI_HOME/; sh bin/dbapi-daemon.sh start gateway;"
 
 # query server status
 echo "query server status"
