f0bbdf3a58cdab6618061199c5955974a06b13d4
jiangqiang <1032355091@qq.com>
2022-02-02 10:39:44 +0800

    集群脚本

--- dbapi-assembly/bin/status-all.sh
+++ dbapi-assembly/bin/status-all.sh
@@ -8,13 +8,13 @@ source $DBAPI_HOME/conf/install_config.conf
 apiServerHost=(${apiServers//,/ })
 for host in ${apiServerHost[@]}
 do
-  echo $host
+
 	state=`ssh -p $sshPort $host "cd $DBAPI_HOME/; sh bin/dbapi-daemon.sh status apiServer;"`
+	echo "$host $state"
 done
 
-echo $manager
 state=`ssh -p $sshPort $manager  "cd $DBAPI_HOME/; sh bin/dbapi-daemon.sh status manager;"`
+echo "$manager $state"
 
-echo $gateway
 state=`ssh -p $sshPort $gateway  "cd $DBAPI_HOME/; sh bin/dbapi-daemon.sh status gateway;"`
-
+echo "$gateway $state"
