a985d5ec314042f43b1ee46b8e712208beb120d1
jiangqiang <jiangqiang110@126.com>
2022-02-18 20:21:51 +0800

    docker

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,7 @@ FROM freakchicken/java8:1.0
 
 ARG VERSION
 ENV DOCKER true
+ENV ROLE standalone
 
 ADD dist/DBApi-${VERSION}-bin.tar.gz /opt/
 
@@ -14,4 +15,4 @@ RUN sh /opt/init_config.sh
 
 EXPOSE 8520 8523 8524 8525
 
-ENTRYPOINT sh bin/dbapi.sh start standalone
+ENTRYPOINT sh bin/dbapi.sh start $ROLE
--- docker/build.sh
+++ docker/build.sh
@@ -6,4 +6,4 @@ echo $ROOT_DIR
 cd $ROOT_DIR
 VERSION=$(mvn -q -DforceStdout -N org.apache.maven.plugins:maven-help-plugin:3.2.0:evaluate -Dexpression=project.version)
 TAG=${TAG:-"$VERSION"}
-docker build --build-arg VERSION=$VERSION -t freakchicken/db-api:$TAG "$ROOT_DIR"/docker/
+docker build --build-arg VERSION=$VERSION -t freakchicken/db-api:$TAG "$ROOT_DIR"
