fe10e0d8c302d06b28cb3fed77c27c3acb17a811
freakchicken <jiangqiang110@126.com>
2022-02-19 07:34:52 +0000

    docker push

--- docker/build.sh
+++ docker/build.sh
@@ -2,7 +2,6 @@
 
 DIR=$(dirname "$0")/..
 ROOT_DIR=`cd "$DIR"; pwd`
-echo $ROOT_DIR
 cd $ROOT_DIR
 VERSION=$(mvn -q -DforceStdout -N org.apache.maven.plugins:maven-help-plugin:3.2.0:evaluate -Dexpression=project.version)
 TAG=${TAG:-"$VERSION"}
---
+++ docker/push.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+DIR=$(dirname "$0")/..
+ROOT_DIR=`cd "$DIR"; pwd`
+cd $ROOT_DIR
+VERSION=$(mvn -q -DforceStdout -N org.apache.maven.plugins:maven-help-plugin:3.2.0:evaluate -Dexpression=project.version)
+TAG=${TAG:-"$VERSION"}
+
+docker push freakchicken/db-api:$TAG
