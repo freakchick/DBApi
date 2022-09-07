f11407815092633027587355bfe438dcd92fc95d
freakchicken <jiangqiang110@126.com>
2022-09-07 06:53:03 +0000

    加密解密优化，适配jdk11+
    Signed-off-by: freakchicken <jiangqiang110@126.com>

--- dbapi-service/src/main/java/com/gitee/freakchicken/dbapi/basic/util/DESUtils.java
+++ dbapi-service/src/main/java/com/gitee/freakchicken/dbapi/basic/util/DESUtils.java
@@ -1,31 +1,25 @@
 package com.gitee.freakchicken.dbapi.basic.util;
 
 import org.apache.commons.codec.binary.Base64;
-import org.apache.commons.io.IOUtils;
-import sun.misc.BASE64Decoder;
-import sun.misc.BASE64Encoder;
 
-import javax.crypto.Cipher;
-import javax.crypto.SecretKey;
-import javax.crypto.SecretKeyFactory;
-import javax.crypto.spec.DESKeySpec;
 import java.io.IOException;
-import java.security.*;
 
 public class DESUtils {
 
-    private final static String DES = "DES";
     private final static String ENCODE = "UTF-8";
-
-    private final static String DES_KEY = PropertiesUtil.getKey("dbapi.secret.key");
 
     public static void main(String[] args) {
         String pass = "root123456";
-        System.out.println("加密前：    " + pass);
+
+
         try {
-            String encrypt = encrypt(pass);
-            System.err.println("加密后：    " + encrypt);
-            System.err.println("解密后：    " + decrypt(encrypt));
+            String s = Base64.encodeBase64String(pass.getBytes(ENCODE));
+            System.out.println(s);
+
+            byte[] bytes = Base64.decodeBase64(s);
+            String s1 = new String(bytes, ENCODE);
+            System.out.println(s1);
+
         } catch (Exception e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
@@ -41,9 +35,7 @@ public class DESUtils {
      * @throws Exception
      */
     public static String encrypt(String data) throws Exception {
-        byte[] bt = encrypt(data.getBytes(ENCODE), DES_KEY.getBytes(ENCODE));
-        String strs = new BASE64Encoder().encode(bt);
-        return strs;
+        return Base64.encodeBase64String(data.getBytes(ENCODE));
     }
 
     /**
@@ -57,67 +49,8 @@ public class DESUtils {
     public static String decrypt(String data) throws Exception {
         if (data == null)
             return null;
-        BASE64Decoder decoder = new BASE64Decoder();
-        byte[] buf = decoder.decodeBuffer(data);
-        byte[] bt = decrypt(buf, DES_KEY.getBytes(ENCODE));
-        return new String(bt, ENCODE);
+        byte[] bytes = Base64.decodeBase64(data);
+        return new String(bytes, ENCODE);
     }
-
-    /**
-     * Description 根据键值进行加密
-     *
-     * @param data
-     * @param key  加密键byte数组
-     * @return
-     * @throws Exception
-     */
-    private static byte[] encrypt(byte[] data, byte[] key) throws Exception {
-        // 生成一个可信任的随机数源
-        SecureRandom sr = new SecureRandom();
-
-        // 从原始密钥数据创建DESKeySpec对象
-        DESKeySpec dks = new DESKeySpec(key);
-
-        // 创建一个密钥工厂，然后用它把DESKeySpec转换成SecretKey对象
-        SecretKeyFactory keyFactory = SecretKeyFactory.getInstance(DES);
-        SecretKey securekey = keyFactory.generateSecret(dks);
-
-        // Cipher对象实际完成加密操作
-        Cipher cipher = Cipher.getInstance(DES);
-
-        // 用密钥初始化Cipher对象
-        cipher.init(Cipher.ENCRYPT_MODE, securekey, sr);
-
-        return cipher.doFinal(data);
-    }
-
-    /**
-     * Description 根据键值进行解密
-     *
-     * @param data
-     * @param key  加密键byte数组
-     * @return
-     * @throws Exception
-     */
-    private static byte[] decrypt(byte[] data, byte[] key) throws Exception {
-        // 生成一个可信任的随机数源
-        SecureRandom sr = new SecureRandom();
-
-        // 从原始密钥数据创建DESKeySpec对象
-        DESKeySpec dks = new DESKeySpec(key);
-
-        // 创建一个密钥工厂，然后用它把DESKeySpec转换成SecretKey对象
-        SecretKeyFactory keyFactory = SecretKeyFactory.getInstance(DES);
-        SecretKey securekey = keyFactory.generateSecret(dks);
-
-        // Cipher对象实际完成解密操作
-        Cipher cipher = Cipher.getInstance(DES);
-
-        // 用密钥初始化Cipher对象
-        cipher.init(Cipher.DECRYPT_MODE, securekey, sr);
-
-        return cipher.doFinal(data);
-    }
-
 
 }
