0cb18b4ded9babfbe9fc3e524a104ac76f189947
danny <jiangqiang110@126.com>
2022-10-31 10:07:37 +0800

    kafka writer

--- dbapi-service/src/main/java/com/gitee/freakchicken/dbapi/basic/log/AccessLogKafkaWriter.java
+++ dbapi-service/src/main/java/com/gitee/freakchicken/dbapi/basic/log/AccessLogKafkaWriter.java
@@ -4,12 +4,22 @@ import com.alibaba.fastjson.JSON;
 import com.gitee.freakchicken.dbapi.basic.domain.AccessLog;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
 
 @Component
 @ConditionalOnProperty(value = "access.log.writer",havingValue = "kafka")
 public class AccessLogKafkaWriter implements AccessLogWriter{
+
+    @Autowired
+    private KafkaTemplate<String, String> template;
+
+    @Value("${access.log.kafka.topic}")
+    private String accessLogTopic;
+
     @Override
     public void write(AccessLog log) {
-        System.out.println(JSON.toJSONString(log));
+        template.send(accessLogTopic, JSON.toJSONString(log));
     }
 }
