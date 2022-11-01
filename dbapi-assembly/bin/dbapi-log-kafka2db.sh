d43ac91766bedc6e780909775120cee9602a165d
danny <jiangqiang110@126.com>
2022-11-01 11:28:32 +0800

    kafka2db script

---
+++ dbapi-assembly/bin/dbapi-log-kafka2db.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+usage="Usage: log-kafka2db.sh <group>"
+
+# if no args specified, show usage
+if [ $# -le 1 ]; then
+  echo $usage
+  exit 1
+fi
+
+startStop=$1
+shift
+group=$1
+shift
+
+BIN_DIR=`dirname $0`
+BIN_DIR=`cd "$BIN_DIR"; pwd`
+DBAPI_HOME=`cd "$BIN_DIR/.."; pwd`
+
+export DBAPI_CONF_DIR=$DBAPI_HOME/conf
+export DBAPI_LIB_DIR=$DBAPI_HOME/lib/
+
+CLASS=com.gitee.freakchicken.dbapi.basic.log.AccessLogKafkaReader
+
+command="java -cp $DBAPI_LIB_DIR/*:$DBAPI_CONF_DIR $CLASS"
+nohup $command > /dev/null 2>&1 &
