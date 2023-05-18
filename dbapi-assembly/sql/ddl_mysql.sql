76b50897fd0827d2715d791b82ae52d7315faa52
jiangqiang <jiangqiang110@126.com>
2023-05-18 10:52:11 +0800

    sql script

--- dbapi-assembly/sql/ddl_mysql.sql
+++ dbapi-assembly/sql/ddl_mysql.sql
@@ -1,16 +1,7 @@
-DROP TABLE IF EXISTS `client_auth`;
-
-CREATE TABLE `client_auth`
-(
-    `id`        int(11) NOT NULL AUTO_INCREMENT,
-    `client_id` varchar(20) DEFAULT NULL,
-    `group_id`  varchar(20) DEFAULT NULL,
-    PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
-
-
+-- ----------------------------
+-- Table structure for api_config
+-- ----------------------------
 DROP TABLE IF EXISTS `api_config`;
-
 CREATE TABLE `api_config`
 (
     `id`             varchar(20) NOT NULL,
@@ -27,30 +18,13 @@ CREATE TABLE `api_config`
     `create_user_id` int(11) DEFAULT NULL,
     `create_time`    varchar(20)  DEFAULT NULL,
     `update_time`    varchar(20)  DEFAULT NULL,
-    PRIMARY KEY (`id`),
-    UNIQUE KEY `path` (`path`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+    PRIMARY KEY (`id`) USING BTREE,
+    UNIQUE INDEX `path`(`path`) USING BTREE
+) ENGINE = InnoDB DEFAULT CHARSET=utf8;
 
-
-DROP TABLE IF EXISTS `api_plugin_config`;
-CREATE TABLE `api_plugin_config`
-(
-    `api_id`       varchar(20) NOT NULL,
-    `plugin_type`  int(11) DEFAULT NULL,
-    `plugin_name`  varchar(255) DEFAULT NULL,
-    `plugin_param` text
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-DROP TABLE IF EXISTS `api_history`;
-CREATE TABLE `api_history`
-(
-    `id`          int(11) NOT NULL AUTO_INCREMENT,
-    `api_id`      varchar(20) DEFAULT NULL,
-    `content`     text,
-    `create_time` varchar(20) DEFAULT NULL,
-    PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
+-- ----------------------------
+-- Table structure for api_group
+-- ----------------------------
 DROP TABLE IF EXISTS `api_group`;
 CREATE TABLE `api_group`
 (
@@ -59,68 +33,39 @@ CREATE TABLE `api_group`
     `create_user_id` int(11) DEFAULT NULL,
     `create_time`    varchar(20)  DEFAULT NULL,
     `update_time`    varchar(20)  DEFAULT NULL,
-    PRIMARY KEY (`id`),
-    UNIQUE KEY `name` (`name`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+    PRIMARY KEY (`id`) USING BTREE,
+    UNIQUE INDEX `name`(`name`) USING BTREE
+) ENGINE = InnoDB DEFAULT CHARSET=utf8;
 
-
-DROP TABLE IF EXISTS `datasource`;
-
-CREATE TABLE `datasource`
-(
-    `id`             varchar(255) NOT NULL,
-    `name`           varchar(255) DEFAULT NULL,
-    `note`           varchar(255) DEFAULT NULL,
-    `type`           varchar(255) DEFAULT NULL,
-    `url`            varchar(255) DEFAULT NULL,
-    `username`       varchar(255) DEFAULT NULL,
-    `password`       varchar(255) DEFAULT NULL,
-    `driver`         varchar(100) DEFAULT NULL,
-    `table_sql`      varchar(255) DEFAULT NULL,
-    `create_user_id` int(11) DEFAULT NULL,
-    `create_time`    varchar(20)  DEFAULT NULL,
-    `update_time`    varchar(20)  DEFAULT NULL,
-
-    PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-
-DROP TABLE IF EXISTS `firewall`;
-
-CREATE TABLE `firewall`
-(
-    `status` varchar(255) DEFAULT NULL,
-    `mode`   varchar(255) DEFAULT NULL
-) ENGINE = InnoDB
-  DEFAULT CHARSET = utf8;
-
-
-DROP TABLE IF EXISTS `ip_rules`;
-
-CREATE TABLE `ip_rules`
-(
-    `type` varchar(255)   DEFAULT NULL,
-    `ip`   varchar(10240) DEFAULT NULL
-) ENGINE = InnoDB
-  DEFAULT CHARSET = utf8;
-
-
-DROP TABLE IF EXISTS `user`;
-
-CREATE TABLE `user`
+-- ----------------------------
+-- Table structure for api_history
+-- ----------------------------
+DROP TABLE IF EXISTS `api_history`;
+CREATE TABLE `api_history`
 (
     `id`          int(11) NOT NULL AUTO_INCREMENT,
-    `username`    varchar(255) DEFAULT NULL,
-    `password`    varchar(255) DEFAULT NULL,
-    `type`        int(11) DEFAULT NULL,
-    `email`       varchar(255) DEFAULT NULL,
-    `create_time` varchar(20)  DEFAULT NULL,
-    PRIMARY KEY (`id`),
-    UNIQUE KEY `username` (`username`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+    `api_id`      varchar(20) DEFAULT NULL,
+    `content`     text,
+    `create_time` varchar(20) DEFAULT NULL,
+    PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 DEFAULT CHARSET=utf8;
 
+-- ----------------------------
+-- Table structure for api_plugin_config
+-- ----------------------------
+DROP TABLE IF EXISTS `api_plugin_config`;
+CREATE TABLE `api_plugin_config`
+(
+    `api_id`       varchar(20) NOT NULL,
+    `plugin_type`  int(11) DEFAULT NULL,
+    `plugin_name`  varchar(255) DEFAULT NULL,
+    `plugin_param` text
+) ENGINE = InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for client
+-- ----------------------------
 DROP TABLE IF EXISTS `client`;
-
 CREATE TABLE `client`
 (
     `id`              varchar(255) NOT NULL DEFAULT '',
@@ -134,9 +79,99 @@ CREATE TABLE `client`
     `create_user_id`  int(11) DEFAULT NULL,
     `create_time`     varchar(20)           DEFAULT NULL,
     `update_time`     varchar(20)           DEFAULT NULL,
-    PRIMARY KEY (`id`),
-    UNIQUE KEY `id` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+    PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for client_auth
+-- ----------------------------
+DROP TABLE IF EXISTS `client_auth`;
+CREATE TABLE `client_auth`
+(
+    `id`        int(11) NOT NULL AUTO_INCREMENT,
+    `client_id` varchar(20) DEFAULT NULL,
+    `group_id`  varchar(20) DEFAULT NULL,
+    PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for datasource
+-- ----------------------------
+DROP TABLE IF EXISTS `datasource`;
+CREATE TABLE `datasource`
+(
+    `id`             varchar(255) NOT NULL,
+    `name`           varchar(255) DEFAULT NULL,
+    `note`           varchar(255) DEFAULT NULL,
+    `type`           varchar(255) DEFAULT NULL,
+    `url`            varchar(255) DEFAULT NULL,
+    `username`       varchar(255) DEFAULT NULL,
+    `password`       varchar(255) DEFAULT NULL,
+    `driver`         varchar(100) DEFAULT NULL,
+    `table_sql`      varchar(255) DEFAULT NULL,
+    `create_user_id` int(11) DEFAULT NULL,
+    `update_time`    varchar(20)  DEFAULT NULL,
+    `create_time`    varchar(20)  DEFAULT NULL,
+    PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for firewall
+-- ----------------------------
+DROP TABLE IF EXISTS `firewall`;
+CREATE TABLE `firewall`
+(
+    `status` varchar(255) DEFAULT NULL,
+    `mode`   varchar(255) DEFAULT NULL
+) ENGINE = InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for ip_rules
+-- ----------------------------
+DROP TABLE IF EXISTS `ip_rules`;
+CREATE TABLE `ip_rules`
+(
+    `type` varchar(255) DEFAULT NULL,
+    `ip`   text         DEFAULT NULL
+) ENGINE = InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for perm_group_datasource
+-- ----------------------------
+DROP TABLE IF EXISTS `perm_group_datasource`;
+CREATE TABLE `perm_group_datasource`
+(
+    `group_id`      varchar(20) NOT NULL,
+    `datasource_id` varchar(20) DEFAULT NULL,
+    `type`          int(11) DEFAULT NULL
+) ENGINE = InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for perm_user_group
+-- ----------------------------
+DROP TABLE IF EXISTS `perm_user_group`;
+CREATE TABLE `perm_user_group`
+(
+    `user_id`  int(11) NOT NULL,
+    `group_id` varchar(20) NOT NULL
+) ENGINE = InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for user
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user`
+(
+    `id`          int(11) NOT NULL AUTO_INCREMENT,
+    `username`    varchar(255) DEFAULT NULL,
+    `password`    varchar(255) DEFAULT NULL,
+    `role`        varchar(20)  DEFAULT NULL,
+    `email`       varchar(255) DEFAULT NULL,
+    `create_time` varchar(20)  DEFAULT NULL,
+    PRIMARY KEY (`id`) USING BTREE,
+    UNIQUE INDEX `username`(`username`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 DEFAULT CHARSET=utf8;
+
 
 insert into `firewall`(`status`, `mode`)
 values ('off', 'black');
@@ -145,5 +180,5 @@ insert into `ip_rules`(`type`, `ip`)
 values ('white', NULL),
        ('black', NULL);
 
-insert into `user`(`id`, `username`, `password`, `type`)
-values (1, 'admin', '21232f297a57a5a743894a0e4a801fc3', 1);
+insert into `user`(`id`, `username`, `password`, `role`)
+values (1, 'admin', '21232f297a57a5a743894a0e4a801fc3', 'ADMIN');
