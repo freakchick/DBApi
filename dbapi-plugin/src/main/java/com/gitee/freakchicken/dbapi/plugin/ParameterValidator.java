c028bd66f36a717ae6fd31f83eb40b37d2b36922
jiangqiang <jiangqiang110@126.com>
2023-05-18 08:18:35 +0800

    parameter validator

--- dbapi-plugin/src/main/java/com/gitee/freakchicken/dbapi/plugin/ParameterValidator.java
+++ dbapi-plugin/src/main/java/com/gitee/freakchicken/dbapi/plugin/ParameterValidator.java
@@ -2,6 +2,7 @@ package com.gitee.freakchicken.dbapi.plugin;
 
 import java.util.Map;
 
+import com.gitee.freakchicken.dbapi.common.ApiConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -15,11 +16,13 @@ public abstract class ParameterValidator implements BasePlugin {
     public abstract void init();
 
     /**
-     * 数据转换逻辑
+     * 请求参数校验逻辑
      *
      * @param requestParameter API请求参数
-     * @param params           全局转换插件的局部参数
+     * @param apiConfig        API元数据
+     * @param clientId         客户端ID
+     * @param params           插件的局部参数
      * @return
      */
-    public abstract boolean validate(Map<String, Object> requestParameter, String params);
+    public abstract boolean validate(Map<String, Object> requestParameter, ApiConfig apiConfig, String clientId, String params);
 }
